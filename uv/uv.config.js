cecbe5a211121a1e88158e13eef452ec2280f88e
posh <101807149+ImPosh@users.noreply.github.com>
2024-01-16 13:21:00 -0500

    Update uv.config.js

--- uv/uv.config.js
+++ uv/uv.config.js
@@ -1,6 +1,6 @@
 self.__uv$config = {
     prefix: '/uv/service/',
-    bare: 'some sort of bare server',
+    bare: 'https://refactored-space-broccoli-jpgv9544vxg2rg6-8080.app.github.dev/v/',
     encodeUrl: Ultraviolet.codec.xor.encode,
     decodeUrl: Ultraviolet.codec.xor.decode,
     handler: '/uv/uv.handler.js',
